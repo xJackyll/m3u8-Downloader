09c65fe6b43250cf6e698a024647263dd0112b22
xJackyll <146102162+xJackyll@users.noreply.github.com>
2024-01-20 02:42:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 import os
 import re
-
+from moviepy.editor import *
+from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips
 
 def is_valid_url(url):
     try:
@@ -50,30 +51,24 @@ def find_master_m3u8_link(url, xpath, chromedir, user_agent):
         # Getting the url of the stream
         driver.get(url)
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
-        perf = driver.get_log('performance')
+        logs = driver.get_log('performance')
 
         # The first pattern we search for is 'playlist.m3u8'
-        pattern = re.compile(r'playlist\.m3u8')
-
         # The second pattern we search for starts with 'https' and finished with "V001"
+        pattern = re.compile(r'playlist\.m3u8')
         pattern2 = re.compile(r'https://\S+(?=V001)')
 
-
         # We loop into the log of the driver searching for the two patterns above
-        for i in perf:
+        for log in logs:
 
             # Every element in the list is a nested dictionary. we only look in the values associated with the key 'message'
-            a = i['message']
+            message = log['message']
 
             # Check if the string matches the first pattern
-            if pattern.search(a):
-
-                # After the previous check, we expect that there are matches in the following block of code.
-                # We use the first match and then break from the code
-                match = pattern2.findall(a)
+            # When we match the 2nd pattern we store the link and then break from the code
+            if pattern.search(message):
+                main_m3u8_link = pattern2.findall(message)[0]
                 break
-
-        main_m3u8_link = match[0]
 
         # Check if the variable is not empy
         if not main_m3u8_link:
@@ -90,6 +85,7 @@ def find_master_m3u8_link(url, xpath, chromedir, user_agent):
 
 def get_master_m3u8(url):
     ResolutionsList = []
+    AudioList = []
 
     # Reading the m3u8 master file
     r = requests.get(url)
@@ -108,13 +104,15 @@ def get_master_m3u8(url):
 
     # The user chooses a resolution and the script checks if the input is correct
     tries = 0
-    while tries < 3:
+    exit_loop = False
+    while tries < 3 and exit_loop is False:
         try:
-            chosen_res = int(input("what resolution you want? (Enter the associated number) "))
-            if index > chosen_res >= 0:
+            chosen_res_num = int(input("what resolution you want? (Enter the associated number) "))
+            if index > chosen_res_num >= 0:
 
                 # Return the filename of the chosen resolution
-                return ResolutionsList[chosen_res][0]
+                ResChose = ResolutionsList[chosen_res_num][0]
+                exit_loop = True
 
             else:
                 print("Invalid input. Please enter a valid integer.")
@@ -124,8 +122,50 @@ def get_master_m3u8(url):
         tries += 1
 
     # If we reach the maximum number of tries the script exits
-    print(f"Maximum attempts reached. Exiting the program.")
-    exit()
+    if tries == 3:
+        print(f"Maximum attempts reached. Exiting the program.")
+        exit()
+
+    # We display every possible audio, if there is any
+    if len(m3u8_master.data['media']) != 0:
+        index = 0
+        print("\nAudio:")
+        for media in m3u8_master.data['media']:
+            if media['default'] == "YES":
+                print(f"{media['name']} ({index}) Default: {media['default']}")
+            else:
+                print(f"{media['name']} ({index})")
+            AudioList.append([media['uri'], media['name']])
+
+            index += 1
+        # The user chooses a resolution and the script checks if the input is correct
+        tries = 0
+        exit_loop = False
+        while tries < 3 and exit_loop is False:
+            try:
+                chosen_audio_num = int(input("what resolution you want? (Enter the associated number) "))
+                if index > chosen_audio_num >= 0:
+
+                    # Return the filename of the chosen resolution
+                    AudioChose = AudioList[chosen_audio_num][0]
+                    exit_loop = True
+
+                else:
+                    print("Invalid input. Please enter a valid integer.")
+            except ValueError:
+                print("Invalid input. Please enter a valid integer.")
+
+            tries += 1
+
+    else:
+        AudioChose = None
+
+    # If we reach the maximum number of tries the script exits
+    if tries == 3:
+        print(f"Maximum attempts reached. Exiting the program.")
+        exit()
+
+    return ResChose, AudioChose
 
 
 def get_chosen_m3u8(url, name_chosen_m3u8, path_to_file):
@@ -147,6 +187,16 @@ def get_chosen_m3u8(url, name_chosen_m3u8, path_to_file):
             index += 1
             print(f"Progress: {round(index / num_seg * 100, 1)} %")
 
+
+def merge_vid_n_audio(video, audio, merged_video):
+    videoclip = VideoFileClip(video)
+    audioclip = AudioFileClip(audio)
+
+    # Concatenate the video clip with the audio clip
+    final_clip = videoclip.set_audio(audioclip)
+
+    # Export the final video with audio
+    final_clip.write_videofile(merged_video)
 
 def main():
     # Declaring and checking variables
@@ -187,16 +237,39 @@ def main():
         ChromeUser_Dir = os.path.join(os.environ['LOCALAPPDATA'], 'Google', 'Chrome', 'User Data', 'Default') # This is an exmple, put your own Chrome User Profile
         user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
         file_path = os.path.realpath(__file__)
-        StoringPath = f"{file_path}\\..\\..\\VideoDownloads\\{FileName}.ts"
+        StoringPath = f"{file_path}\\..\\..\\VideoDownloads\\{FileName}"
+        VideoPath = f"{StoringPath}.ts"
+        AudioPath = f"{StoringPath}.mp3"
+        VidAndAudio = f"{StoringPath}.mp4"
 
         main_m3u8_link = find_master_m3u8_link(Url, VideoPlayer_Full_XPATH, ChromeUser_Dir, user_agent)
 
         # Reads the m3m8 master file and retrieves the resolution
-        m3u8_Name = get_master_m3u8(main_m3u8_link)
+        m3u8_name, m3u8_audio = get_master_m3u8(main_m3u8_link)
 
-        # Reads the m3m8 file we have chosen and downloads the .ts file in the path chosen
-        splitted_url = url_splitter(main_m3u8_link)  # This is the "folder" in witch all the files we work with are stored
-        get_chosen_m3u8(splitted_url, m3u8_Name, StoringPath)
+        # This is the "folder" in witch all the files we work with are stored on the site
+        splitted_url = url_splitter(main_m3u8_link)
+
+        # Reads the m3m8 file we have chosen and downloads the file/files in the path chosen
+        # If we have just one audio the work is done
+        print("Video")
+        get_chosen_m3u8(splitted_url, m3u8_name, VideoPath)
+
+        # If we also have the audio we have to put it with the video in one singular file
+        if m3u8_audio is not None:
+            print("Audio")
+            get_chosen_m3u8(splitted_url, m3u8_audio, AudioPath)
+            merge_vid_n_audio(VideoPath, AudioPath, VidAndAudio)
+
+            # We delete the two files we just merged
+            if os.path.exists(VideoPath) and os.path.exists(AudioPath):
+                os.remove(VideoPath)
+                os.remove(AudioPath)
+            else:
+                print("There is some issue when locating the audio and video file. It seems they does not exist.")
+
+        print("The video is READY!!!")
+
     except ValueError as e:
         print(f"Error: {e}")
         exit()
